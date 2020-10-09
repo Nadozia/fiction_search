d2c63cba45e800bfdae4942dc9479c2cac077736
Samuel <42663794+saboshi69@users.noreply.github.com>
2020-10-10 00:30:11 +0800

    var > let

--- jsSearch.ts
+++ jsSearch.ts
@@ -18,15 +18,15 @@ async function getData() {
                 addChild(s)
             })
         } else {
-            var row = document.createElement('li')
-            var s = document.createTextNode('查無此詞')
+            let row = document.createElement('li')
+            let s = document.createTextNode('查無此詞')
             row.appendChild(s)
             table.appendChild(row)
         }
 
     } else {
-        var row = document.createElement('li')
-        var s = document.createTextNode('請輸入關鍵字')
+        let row = document.createElement('li')
+        let s = document.createTextNode('請輸入關鍵字')
         row.appendChild(s)
         table.appendChild(row)
     }
@@ -34,8 +34,8 @@ async function getData() {
 }
 
 function addChild(sentense:string) {
-    var row = document.createElement('li')
-    var s = document.createTextNode(sentense)
+    let row = document.createElement('li')
+    let s = document.createTextNode(sentense)
 
     row.appendChild(s)
     row.innerHTML = row.innerHTML.replace(keyword.value, `<mark>${keyword.value}</mark>`)
@@ -43,9 +43,9 @@ function addChild(sentense:string) {
 }
 
 function regex(txt:string) {
-    var str_patt = `.{${char_num.value}}${keyword.value}.{${char_num.value}}`
-    var patt = new RegExp(str_patt, 'g');
-    var results = txt.match(patt);
+    let str_patt = `.{${char_num.value}}${keyword.value}.{${char_num.value}}`
+    let patt = new RegExp(str_patt, 'g');
+    let results = txt.match(patt);
     return results;
 }
 
